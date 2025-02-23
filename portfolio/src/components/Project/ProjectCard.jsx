836cc31d997acae2cd01401a01b0112d933a206a
pardeep-pathania <pathanianikhil145@gmail.com>
2025-02-23 23:54:05 +0530

    add a logic behind the source code button

--- portfolio/src/components/Project/ProjectCard.jsx
+++ portfolio/src/components/Project/ProjectCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-function ProjectCard({title, main, imageUrl}) {
+function ProjectCard({title, main, imageUrl, gitRepoUrl}) {
+
 
   const imageStyle = {
     width: '250px', 
     height: '125px', 
     objectFit: 'contain', 
   };
+
+  const handleOpenRepo = () => {
+    window.open(gitRepoUrl, "_blank"); // Opens the URL in a new tab
+  };
+
 
   return (
     <div className='p-3 md:p-6 flex flex-col w-80 bg-[#030626] inset-shadow-slate-900 rounded-2xl'>
@@ -17,9 +23,9 @@ function ProjectCard({title, main, imageUrl}) {
       <p className='px-4 text-sm md:p-4 flex gap-2 md:gap-4'>{main}</p>
       <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
         <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85
-        duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Demo</button>
+        duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]' onClick={handleOpenRepo}>Demo</button>
         <button className='md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85
-        duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Source Code</button>
+        duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]' onClick={handleOpenRepo}>Source Code</button>
       </div>
     </div>
   )
--- portfolio/src/components/Project/Projects.jsx
+++ portfolio/src/components/Project/Projects.jsx
@@ -5,6 +5,12 @@ import Spotify from "../../assets/Spotify.png";
 import ProjectCard from './ProjectCard'
 
 function Project() {
+
+  const gitRepoNews = "https://github.com/Pardeep-pathania/Newsly";
+  const gitRepoSpotify = "https://github.com/Pardeep-pathania/Spotify-clone";
+  const gitRepoPaste = "https://github.com/Pardeep-pathania/Paste-App";
+
+
   return (
     <div id='Projects' className='max-sm:mt-10 sm:mt-10 p-10 md:p-24 text-white'>
       <h1 className='text-2xl md:text-4xl text-white font-bold'>Projects</h1>
@@ -12,19 +18,24 @@ function Project() {
       <ProjectCard
        title="News App" 
        imageUrl={news}
-       main="This is a News App whose name Newsly created in React Js and Bootstrap by using API key to fetch data from the server."/>
+       main="This is a News App whose name Newsly created in React Js and Bootstrap by using API key to fetch data from the server."
+       gitRepoUrl={gitRepoNews}
+       />
       
       <ProjectCard
       title="Paste App" 
       imageUrl={paste}
       main="This is a Paste App which is used to save the data or task with their title in the local storage of the app
-      and can be view, copy, delete and update from the app. Created in React Js and styling using Tailwind CSS and made it full responsive."/>
+      and can be view, copy, delete and update from the app. Created in React Js and styling using Tailwind CSS and made it full responsive."
+      gitRepoUrl={gitRepoPaste}
+      />
       
       <ProjectCard 
       title="Spotify Clone" 
       imageUrl={Spotify}
       main="This is a Spotify Clone in which the feature will be added to play and access the song from the folder 
-      which can be play, pause, next and previous song."/>
+      which can be play, pause, next and previous song."
+      gitRepoUrl={gitRepoSpotify}/>
     </div>
     </div>
   )
