12a366d39b2a87d06e68f12a8389eae0cdbd0f1c
pardeep-pathania <pathanianikhil145@gmail.com>
2025-02-24 16:27:57 +0530

    add a resume download button

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import pic from "../assets/pic.jpg"
 import TextChange from '../TextChange'
+// import Paredeep_Resume from '..assets/Paredeep_Resume.pdf'
 
 function Home() {
+
+    // method to call 
 
     const phoneNumber = '9797607301'; 
 
     const handleCall = () => {
+         // Open the phone Number in a new tab
         window.location.href = `tel:${phoneNumber}`;
     };
+
+
+    // method to download resume
+
+    const handleDownload = () => {
+        const resumeUrl = "https://drive.google.com/file/d/1Nf8TlM2tw5S6i07AtVFpugnVtgXLjm8i/view?usp=sharing"; 
+
+        // Open the resume link in a new tab
+        window.open(resumeUrl, '_blank');
+    };
+
 
 
     return (
@@ -19,8 +34,12 @@ function Home() {
                     a passion for building scalable web applications. My experience spans both front-end and back-end development,
                      allowing me to create seamless user experiences and robust server-side logic. I am dedicated to writing clean,
                      maintainable code and continuously improving my skills.</p>
-                <button onClick={handleCall} className='mt-5 md:md-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:bg-blue-800
+                     <div>
+                <button onClick={handleCall} className='mt-5 md:m-8 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:bg-blue-800
                 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#455697]'>Contact Me</button>
+                <button onClick={handleDownload} className='mt-5 max-sm:ml-3 sm:ml-3 md:m-2 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:bg-blue-800
+                duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#455697]'>Download Resume</button>
+                </div>
             </div>
             <div><img className='mt-7 md:w-90 lg:w-150 lg:mt-15 md:p-5 md:mt-15' src={pic} alt="" /></div>
         </div>
